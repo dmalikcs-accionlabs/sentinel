0043a763101a135a8c580561b1df6207d3a6958b
Deepak Malik <deepak.malik@accionlabs.com>
2020-04-03 16:16:03 +0530

    Email attachemnt save method

--- src/collector/models.py
+++ src/collector/models.py
@@ -6,8 +6,10 @@ import os
 from django.utils.timezone import now
 from azure.servicebus import QueueClient, Message
 import uuid
+from django.core.files.base import ContentFile
+import json
 class EmailBodyTypeChoice:
-    HTML= 'h'
+    HTML = 'h'
     TEXT = 'T'
 
     @classonlymethod
@@ -17,6 +19,7 @@ class EmailBodyTypeChoice:
             (cls.TEXT, 'Text')
         )
 
+
 EMAIL_BODY_TYPE_LIST = EmailBodyTypeChoice.get_choices()
 
 
@@ -24,10 +27,10 @@ class EmailCollection(BaseTimeStampField):
 
     def get_upload_location(instance, filename):
         d = now().strftime("%Y%m%d")
-        return "emails/{}/{}/{}/".format(d, instance.pk, filename)
+        return "emails/{}/{}/{}".format(d, instance.pk, filename)
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    location = models.FileField(upload_to=get_upload_location, null=True)
+    location = models.FileField(upload_to=get_upload_location, null=True, blank=True)
     email_from = models.EmailField()
     subject = models.CharField(max_length=256, blank=True)
     body = models.TextField(blank=True)
@@ -39,9 +42,9 @@ class EmailCollection(BaseTimeStampField):
         return str(self.email_from)
 
     class Meta:
-        ordering = ('-created_at', )
+        ordering = ('-created_at',)
 
-    def save(self, *args,  **kwargs):
+    def save(self, *args, **kwargs):
         created = self._state.adding
         if created:
             try:
@@ -53,12 +56,24 @@ class EmailCollection(BaseTimeStampField):
                 print(e)
                 self.is_published = False
         super(EmailCollection, self).save(*args, **kwargs)
+        if created:
+            self.location.save("{}.json".format(self.pk), ContentFile(json.dumps(
+                {
+                    'email_from': self.email_from,
+                    'boyd': self.body,
+                    'subject': self.subject,
+                }
+            )))
 
 
 class EmailAttachment(BaseTimeStampField):
+    def get_upload_location(instance, filename):
+        d = now().strftime("%Y%m%d")
+        return "emails/{}/{}/{}/".format(d, instance.email.pk, filename)
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.ForeignKey(EmailCollection, on_delete=models.CASCADE)
-    location = models.FileField(upload_to='email/attachments/')
+    location = models.FileField(upload_to=get_upload_location)
 
     def __str__(self):
         return str(self.email.email_from)

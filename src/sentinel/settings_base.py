1cd81035890510952c0e623147a0865e3ce18293
Deepak Malik <deepak.malik@accionlabs.com>
2020-04-08 14:54:39 +0530

    added local configuration

--- src/sentinel/settings_base.py
+++ src/sentinel/settings_base.py
@@ -20,7 +20,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'u_sentinel',
         'PASSWORD': 'unix123',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
     }
 }

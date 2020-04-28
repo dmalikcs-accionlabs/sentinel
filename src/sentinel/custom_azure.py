be015caac4680a3e13c8f3618966917070fdc7d5
Deepak Malik <deepak.malik@accionlabs.com>
2020-04-28 17:29:29 +0530

    disabled azure static azure

--- src/sentinel/custom_azure.py
+++ src/sentinel/custom_azure.py
@@ -9,8 +9,8 @@ class AzureMediaStorage(AzureStorage):
     expiration_secs = None
 
 
-class AzureStaticStorage(AzureStorage):
-    account_name = settings.AZURE_STORAGE_ACCOUNT
-    account_key = settings.AZURE_ACCOUNT_KEY
-    azure_container = settings.AZURE_STATIC_CONTAINER
-    expiration_secs = None
+# class AzureStaticStorage(AzureStorage):
+#     account_name = settings.AZURE_STORAGE_ACCOUNT
+#     account_key = settings.AZURE_ACCOUNT_KEY
+#     azure_container = settings.AZURE_STATIC_CONTAINER
+#     expiration_secs = None

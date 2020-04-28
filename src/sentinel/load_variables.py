88500d3b6124458392e0af90ed2b9a6fa8628f88
Deepak Malik <deepak.malik@accionlabs.com>
2020-04-28 17:17:14 +0530

    Azure AD updates

--- src/sentinel/load_variables.py
+++ src/sentinel/load_variables.py
@@ -31,23 +31,7 @@ AZURE_SB_CONN_STRING = client.get_secret('SENTINEL-AZURE-SB-CONN-STRING').value
 
 
 ### Azure AD Services
-TENANT_ID = client.get_secret('SENTINEL-AZURE-TENANT-ID').value
-CLIENT_ID = client.get_secret('SENTINEL-AZURE-CLIENT-ID').value
-RELYING_PARTY_ID = client.get_secret('SENTINEL-AZURE-RELYING-PARTY-ID').value
-AUDIENCE = client.get_secret('SENTINEL-AZURE-AUDIENCE').value
-
-
-##
-
-# SENTINEL-DJANGO-SETTINGS-MODULE = "sentinel.settings",
-# SENTINEL-CELERY-BROKER-URL = "redis://:bXdrR76xUo5bAV4EBk4LCH3LNWVIkPe6ZKH3SWyiLB0=@cardinal-us-alpha-prod-redis-emerald-black-alpha.redis.cache.windows.net:6380/2",
-# SENTINEL-AZURE-STORAGE-ACCOUNT = "83b4a135d5e74f069a8f0d7",
-# SENTINEL-AZURE-ACCOUNT-KEY = "3N+SB3fC5RK6hZPzPUE/mb3Xmt8+lG0WajxMtUCOZZ+m62I595efcQAjD2dpR1suqlJGJGX7hUdRDd6DP0Nk3Q==",
-# SENTINEL-AZURE-CUSTOM-DOMAIN = "83b4a135d5e74f069a8f0d7.blob.core.windows.net",
-# SENTINEL-AZURE-SB-CONN-STRING = "Endpoint=sb://prdmonarchservicebus.servicebus.windows.net/;SharedAccessKeyName=OPSAutomation;SharedAccessKey=EHIvPR82aj/HoRweVcMeBcTydHBooq4Z0zYf8IBtsak=;EntityPath=cancelledorders",
-# SENTINEL-AZURE-SB-CANCEL-QUEUE = "CancelledOrders",
-# SENTINEL-DATABASE-NAME = "sentinel-email-parser-management",
-# SENTINEL-DATABASE-USERNAME = "sentinel_email_parser_readwrite@cardinal-us-alpha-prod-db-emerald-black-alphadbsvr",
-# SENTINEL-DATABASE-PASSWORD = "FCEbY4.=",
-# SENTINEL-DATABASE-HOST = "cardinal-us-alpha-prod-db-emerald-black-alphadbsvr.postgres.database.azure.com"
-# SENTINEL-AZURE-MEDIA-CONTAINER = 'media'
+# TENANT_ID = client.get_secret('SENTINEL-AZURE-TENANT-ID').value
+# CLIENT_ID = client.get_secret('SENTINEL-AZURE-CLIENT-ID').value
+# RELYING_PARTY_ID = client.get_secret('SENTINEL-AZURE-RELYING-PARTY-ID').value
+# AUDIENCE = client.get_secret('SENTINEL-AZURE-AUDIENCE').value
